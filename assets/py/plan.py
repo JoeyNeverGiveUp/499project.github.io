96ee8a58d3b288f956053f1fef45c7b1149b8788
Joey <122506803+JoeyNeverGiveUp@users.noreply.github.com>
2024-03-28 21:39:08 -0400

    Delete assets/py/plan.py

--- assets/py/plan.py
+++
@@ -1,7 +0,0 @@
-from js import document
-
-def display_choice(event):
-                selected_option = document.getElementById('degreePlanSelect').value
-                document.getElementById('selectedPlan').innerText = selected_option
-
-document.getElementById('submit').addEventListener('click', display_choice)
