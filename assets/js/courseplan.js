b12ba99329d43d861e1a60a7621f063305e3a434
Joey <122506803+JoeyNeverGiveUp@users.noreply.github.com>
2024-04-06 23:09:57 -0400

    Update courseplan.js

--- assets/js/courseplan.js
+++ assets/js/courseplan.js
@@ -1,13 +1,24 @@
 var plans = {
-    gbc:"assets/py/Degree-plans-9/gbc.txt",
-    mbc:"assets/py/Degree-plans-9/mbc.txt",
-    sbc:"assets/py/Degree-plans-9/sbc.txt",
-    sca:"assets/py/Degree-plans-9/sca.txt",
-    scm:"assets/py/Degree-plans-9/scm.txt",
-    sco:"assets/py/Degree-plans-9/sco.txt",
-    scs:"assets/py/Degree-plans-9/scs.txt",
-    ssd:"assets/py/Degree-plans-9/ssd.txt",
-    gba:"assets/py/Degree-plans-9/gba.txt"
+    gbc:"../test/assets/py/Degree-plans-9/gbc.txt",
+    mbc:"../test/assets/py/Degree-plans-9/mbc.txt",
+    sbc:"../test/assets/py/Degree-plans-9/sbc.txt",
+    sca:"../test/assets/py/Degree-plans-9/sca.txt",
+    scm:"../test/assets/py/Degree-plans-9/scm.txt",
+    sco:"../test/assets/py/Degree-plans-9/sco.txt",
+    scs:"../test/assets/py/Degree-plans-9/scs.txt",
+    ssd:"../test/assets/py/Degree-plans-9/ssd.txt",
+    gba:"../test/assets/py/Degree-plans-9/gba.txt"
+};
+var visual = {
+    gbc:"../test/v/gbc.png",
+    mbc:"../test/v/mbc.png",
+    sbc:"../test/v/sbc.png",
+    sca:"../test/v/sca.png",
+    scm:"../test/v/scm.png",
+    sco:"../test/v/sco.png",
+    scs:"../test/v/scs.png",
+    ssd:"../test/v/ssd.png",
+    gba:"../test/v/gba.png"
 };
 
 document.getElementById("degreePlanForm").addEventListener("submit", function(event) {
@@ -17,6 +28,8 @@ document.getElementById("degreePlanForm").addEventListener("submit", function(ev
     document.getElementById("selectedPlan").textContent = soname;
     var treemap = plans[so];
     document.getElementById("courseMapLink").href = treemap;
+    var visualmap = visual[so];
+    document.getElementById("visual").href = visualmap;
 });
 
 
