ace35f7d0ffb4ea1a0ae504b2405bd7c7bac5c55
Joey <122506803+JoeyNeverGiveUp@users.noreply.github.com>
2024-04-08 00:01:16 -0400

    Update courseplan.js

--- assets/js/courseplan.js
+++ assets/js/courseplan.js
@@ -1,13 +1,13 @@
 var plans = {
-    gbc:"../test/assets/py/Degree-plans-9/gbc.txt",
-    mbc:"../test/assets/py/Degree-plans-9/mbc.txt",
-    sbc:"../test/assets/py/Degree-plans-9/sbc.txt",
-    sca:"../test/assets/py/Degree-plans-9/sca.txt",
-    scm:"../test/assets/py/Degree-plans-9/scm.txt",
-    sco:"../test/assets/py/Degree-plans-9/sco.txt",
-    scs:"../test/assets/py/Degree-plans-9/scs.txt",
-    ssd:"../test/assets/py/Degree-plans-9/ssd.txt",
-    gba:"../test/assets/py/Degree-plans-9/gba.txt"
+    gbc:"assets/py/Degree-plans-9/gbc.txt",
+    mbc:"assets/py/Degree-plans-9/mbc.txt",
+    sbc:"assets/py/Degree-plans-9/sbc.txt",
+    sca:"assets/py/Degree-plans-9/sca.txt",
+    scm:"assets/py/Degree-plans-9/scm.txt",
+    sco:"assets/py/Degree-plans-9/sco.txt",
+    scs:"assets/py/Degree-plans-9/scs.txt",
+    ssd:"assets/py/Degree-plans-9/ssd.txt",
+    gba:"assets/py/Degree-plans-9/gba.txt"
 };
 var visual = {
     gbc:"v/gbc.png",
